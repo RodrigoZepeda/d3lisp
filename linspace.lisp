f028b524fd1ce52fdcfbdf43ad38cf7b50e6bfa0
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2018-06-13 04:05:13 -0500

    Delete linspace.lisp

--- linspace.lisp
+++
@@ -1,8 +0,0 @@
-(defun linspace (xmin xmax &key (lengthout 100))
-    (let ((x (make-list lengthout :initial-element xmin)) (n (/ (- xmax xmin) (1- lengthout))))
-        (loop for i from 1 to (1- lengthout)
-            do (setf (nth i x) (+ (nth (1- i) x) n))
-        )
-        x
-    )
-)
