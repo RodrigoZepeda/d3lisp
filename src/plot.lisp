327fad3e1cf5b9917bfb4f4be58f9f6cdf4c1da9
Rodrigo Zepeda-Tello <rzepeda17@gmail.com>
2019-09-12 13:07:04 -0500

    improved plot function

--- src/plot.lisp
+++ src/plot.lisp
@@ -1,52 +1,47 @@
 ;;; -*- Mode: LISP; Base: 10; Syntax: ANSI-Common-lisp; Package: D3 -*-
 (in-package #:d3)
 
-(defun createplot  (x y &key (xlab "My x label") (ylab "My y label") (title "My Title") (size 7)
-                            (xmin NIL) (ymin NIL) (xmax NIL) (ymax NIL) (scatter-color "green")
-                            (line-color "purple") (line-width 3) (stroke-fill "none") (line T) (scatter T)
-                            (interpolation "Linear") (showXaxis T) (showYaxis T)
-                            (axisX_label_color "black") (axisY_label_color "black")
-                            (axisX_tick_color "black") (axisY_tick_color "black") (axisX_color "black")
-                            (axisY_color "black") (line-opacity 1) (scatter-opacity 1)
-                            (yaxispos NIL) (xaxispos NIL)
-                            (plotheight "default") (plotwidth "default")
-                            (outercolor "none") (innercolor "none") (annotations NIL)
+(defun plot  (x y &key (x-label "My x label") (y-label "My y label") (title "My Title") (size 7)
+                            (x-minimum NIL) (y-minimum NIL) (x-maximum NIL) (y-maximum NIL)
+                            (scatter-color "green") (line-color "purple") (line-width 3)
+                            (stroke-fill "none") (line T) (scatter T)
+                            (interpolation "Linear") (show-x-axis T) (show-y-axis T)
+                            (axis-x-label-color "black") (axis-y-label-color "black")
+                            (axis-x-tick_color "black") (axis-y-tick-color "black")
+                            (axis-x-color "black")(axis-y-color "black") (line-opacity 1)
+                            (scatter-opacity 1) (y-axis-position NIL) (x-axis-position NIL)
+                            (plot-height "default") (plot-width "default")
+                            (outer-background-color "none") (inner-background-color "none")
+                            (annotations NIL)
                             (annotations-color "black") (annotations-font-size 12)
-                            (title-fontsize 18) (title-color "black")
+                            (title-font-size 18) (title-color "black")
                             (margin (list 10 10 10 10)) (padding (list 30 30 60 60))
-                            (squareplot NIL) (save NIL) (svgname "Myplot")
-                            (filename (concatenate 'string "Myplot" (write-to-string *plot-number*))))
+                            (square-plot NIL) (save NIL) (svg-name "Myplot")
+                            (file-name (concatenate 'string "Myplot" (write-to-string *plot-number*))))
 
     ;;Update plot number
     (setf *plot-number* (1+ *plot-number*))
 
     ;;Call function and write plot to file
     (with-open-file
-      (str (concatenate 'string filename ".html")
+      (str (concatenate 'string file-name ".html")
            :direction :output
            :if-exists :supersede
            :if-does-not-exist :create)
       (format str
             (htmlcode x y
-                :xlab xlab :ylab ylab :title title :size size
-                :xmin xmin :ymin ymin :xmax xmax :ymax ymax :scatter-color scatter-color
+                :x-label x-label :y-label y-label :title title :size size
+                :x-minimum x-minimum :y-minimum y-minimum :x-maximum x-maximum :y-maximum y-maximum :scatter-color scatter-color
                 :line-color line-color :line-width line-width :stroke-fill stroke-fill :line line :scatter scatter
-                :interpolation interpolation :showXaxis showXaxis :showYaxis showYaxis
-                :axisX_label_color axisX_label_color :axisY_label_color axisY_label_color
-                :axisX_tick_color axisX_tick_color :axisY_tick_color axisY_tick_color :axisX_color axisX_color
-                :axisY_color axisY_color :line-opacity line-opacity :scatter-opacity scatter-opacity
-                :xaxispos xaxispos :yaxispos yaxispos
-                :plotheight plotheight :plotwidth plotwidth
-                :outercolor outercolor :innercolor innercolor :annotations annotations
+                :interpolation interpolation :show-x-axis show-x-axis :show-y-axis show-y-axis
+                :axis-x-label-color axis-x-label-color :axis-y-label-color axis-y-label-color
+                :axis-x-tick_color axis-x-tick_color :axis-y-tick-color axis-y-tick-color :axis-x-color axis-x-color
+                :axis-y-color axis-y-color :line-opacity line-opacity :scatter-opacity scatter-opacity
+                :x-axis-position x-axis-position :y-axis-position y-axis-position
+                :plot-height plot-height :plot-width plot-width
+                :outer-background-color outer-background-color :inner-background-color inner-background-color :annotations annotations
                 :annotations-color annotations-color :annotations-font-size annotations-font-size
-                :title-fontsize title-fontsize :title-color title-color
+                :title-font-size title-font-size :title-color title-color
                 :margin margin :padding padding
-                :squareplot squareplot :save save :svgname svgname
-                :plotnum (1- *plot-number*)
-            )
-      )
-    )
-
-
-
-)
+                :square-plot square-plot :save save :svg-name svg-name
+                :plotnum (1- *plot-number*)))))
